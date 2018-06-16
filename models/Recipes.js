a78e24954a7b42184eb8553d665e6502060346dd
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-06-15 22:31:18 -0700

    refactored models

--- models/Recipes.js
+++ models/Recipes.js
@@ -6,19 +6,9 @@ module.exports = function (sequelize, DataTypes) {
         },
         RecipeDescription: {
             type: DataTypes.TEXT,
-            allowNull: false
+            allowNull: true
         }
     });
-
-    Recipe.associate = function (models) {
-        // We're saying that a Post should belong to an Author
-        // A Post can't be created without an Author due to the foreign key constraint
-        Recipe.belongsTo(models.Recipe_Amounts, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
 
     return Recipe;
 };
