535a5f888743e2a1db7ed9404bddb4cee96cdfbf
Luqiang Mao <acmlq@msn.com>
2018-06-17 09:27:13 -0700

    upload

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,25 @@ var router  = express.Router();
 var passport = require("../config/passport");
 var users_controller = require('../controllers/users_controller');
 
-function isLogginIn() {
-    req.loggedin = !!req.user;
-    next();
-}
-//var isAuthenticated = require("../config/middleware/isAuthenticated");
-
+// function isLogginIn() {
+//     req.loggedin = !!req.user;
+//     next();
+// }
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+router.get('/admin', users_controller.adminMain);
 // router.get('/signup', users_controller.registrationPage);
+router.get("/api/posts", );
 
-// router.get('/sign-out', users_controller.signOutUser);
+router.get("/api/posts/:id", );
 
-// router.post('/login', passport.authenticate("local"), users_controller.loginUser);
+router.delete("/api/posts/:id",);
 
-// router.post('/signup', users_controller.signUpUser);
+router.put("/api/posts");
+
+router.get('/sign-out', users_controller.signOutUser);
+
+router.post('/login', passport.authenticate("local"), users_controller.loginUser);
+
+//router.post('/signup', users_controller.signUpUser);
 
 module.exports = router;
