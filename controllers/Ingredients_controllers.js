1927b59bc5c93dc3517406b2ab64afaa3edbaf3a
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-06-18 21:38:31 -0700

    successfully adding to DB

--- controllers/Ingredients_controllers.js
+++ controllers/Ingredients_controllers.js
@@ -2,11 +2,29 @@ var db  = require('../models');
 
 exports.viewIngredients = function(req, res) {
   db.Ingredient.findAll({}).then(function(dbIngredient) {
-    console.log(dbIngredient);
-    // res.render("ingredients", dbIngredient);
     res.render('ingredients', {
       layout: 'main',
       ingredients: dbIngredient
     });
+  });
+};
+
+exports.addIngredient = function(req, res) {
+  console.log(req.body);
+  db.Ingredient.create({
+    IngredientName: req.body.ingredientName,
+    Calories: req.body.Calories,
+    Carbs: req.body.Carbs,
+    Sugar: req.body.Sugar,
+    Fat: req.body.Fat, 
+    Protein: req.body.Protein,
+    isGlutenFree: req.body.isGlutenFree,
+    isNut: req.body.isNut,
+    isGMO: req.body.isGMO,
+    createdAt: "1000-01-01 00:00:00",
+    updatedAt: "1000-01-01 00:00:00"
+  }).then(function(dbIngredient) {
+    console.log(dbIngredient);
+    res.redirect('/ingredients');
   });
 };
---
+++ public/js/ingredientFE.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    var $ingredientNameInput = $("input#ingredientName");
+    var $CaloriesInput = $("input#Calories");
+    var $CarbsInput = $("input#Carbs");
+    var $SugarInput = $("input#Sugar");
+    var $FatInput = $("input#Fat");
+    var $ProteinInput = $("input#Protein");
+    var $isGlutenFreeInput = $("input#isGlutenFree");
+    var $isNutInput = $("input#isNut");
+    var $isGMOInput = $("input#isGMO");
+
+    $(document).on("click", "#submit", insertIngredient);
+
+    function insertIngredient(event) {
+        console.log('CLICKED!');
+        event.preventDefault();
+        var ingredient = {
+            ingredientName: $ingredientNameInput.val().trim(),
+            Calories: $CaloriesInput.val().trim(),
+            Carbs: $CarbsInput.val().trim(),
+            Sugar: $SugarInput.val().trim(),
+            Fat: $FatInput.val().trim(),
+            Protein: $ProteinInput.val().trim(),
+            isGlutenFree: $isGlutenFreeInput.val().trim(),
+            isNut: $isNutInput.val().trim(),
+            isGMO: $isGMOInput.val().trim()
+        };
+        console.log('ingredient: '+ingredient.isNut);
+
+        $.post("/ingredients/new", ingredient);
+
+        $ingredientNameInput.val("");
+        $isGlutenFreeInput.val("");
+        $isNutInput.val("");
+        $isGMOInput.val("");
+        $CaloriesInput.val("");
+        $CarbsInput.val("");
+        $SugarInput.val("");
+        $FatInput.val("");
+        $ProteinInput.val("");
+    }
+
+});
