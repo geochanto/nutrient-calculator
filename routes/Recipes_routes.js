c489a30f858bd73bc62315095cefdc2d79a3af1d
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-06-19 19:36:29 -0700

    updatedRecipefiles

--- routes/Recipes_routes.js
+++ routes/Recipes_routes.js
@@ -5,12 +5,15 @@ var router  = express.Router();
 var Recipes_controllers = require('../controllers/Recipes_controllers');
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-router.get('/', Recipes_controllers.index);
+router.get('/recipes', Recipes_controllers.index);
 
-router.post('/new', isAuthenticated, Recipes_controllers.createRecipes);
+// router.post('/recipes/new', isAuthenticated, Recipes_controllers.createRecipes);
+router.post('/recipes/new', Recipes_controllers.createRecipes);
 
-router.put('/update', isAuthenticated, Recipes_controllers.updateRecipes);
+// router.put('/recipes/update/:id', isAuthenticated, Recipes_controllers.updateRecipes);
+router.put('/recipes/update/:id', Recipes_controllers.updateRecipes);
 
-router.delete('/delete', isAuthenticated, Recipes_controllers.updateRecipes);
+// router.delete('/recipes/delete/:id', isAuthenticated, Recipes_controllers.updateRecipes);
+router.delete('/recipes/delete/:id',Recipes_controllers.updateRecipes);
 
 module.exports = router;
