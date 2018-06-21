ccb64e08e9e98fe620ffff0faa14e4dc0ddeb8fd
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-06-21 03:28:54 -0700

    recipe form front-end dynamic recipe field addition

--- public/js/recipeFE.js
+++ public/js/recipeFE.js
@@ -6,7 +6,38 @@ $(document).ready(function () {
   $(document).on("click", "#submitRecipe", insertRecipe);
   // $(document).on("click", ".editIngredient", editIngredient);
 
+  $(document).on("click", "#addRecipeIngredient", addRecipeIngredient);
 
+  i = 1;
+
+  function addRecipeIngredient () {
+    console.log('clicked');
+    i++;
+    console.log(i);
+    $("#RecipeIngredientGroup").append( `
+    <div class="form-row">
+    <div class="form-group col-md-3" >
+    <label for="RecipeIngredient-${i}">Ingredient ${i}</label>
+    <input type="text" class="form-control" id="RecipeIngredient-${i}" placeholder="Ingredient ${i}">
+    </div>
+
+    <div class="form-group col-md-3">
+    <label for="AmountForSmall-${i}">Amount for Small </label>
+    <input type="text" class="form-control" id="AmountForSmall-${i}" placeholder="Amount for Small">
+  </div>
+
+  <div class="form-group col-md-3">
+    <label for="AmountForMedium-${i}">Amount for Medium </label>
+    <input type="text" class="form-control" id="AmountForMedium-${i}" placeholder="Amount for Medium">
+  </div>
+
+  <div class="form-group col-md-3">
+    <label for="AmountForLarge-${i}">Amount for Large </label>
+    <input type="text" class="form-control" id="AmountForLarge-${i}" placeholder="Amount for Large">
+  </div>
+  </div>` );
+  }
+  
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 function insertRecipe(event) {
       // Make sure to preventDefault on a submit event.
