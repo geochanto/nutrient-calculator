2494cf87beaf204dbcaa9cdee41d7ce0124cc9a9
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-06-21 22:53:18 -0700

    recipe add controller finalized

--- public/js/recipeFE.js
+++ public/js/recipeFE.js
@@ -8,53 +8,9 @@ $(document).ready(function () {
   // $(document).on("click", ".editIngredient", editIngredient);
 
   $(document).on("click", "#addRecipeIngredient", addRecipeIngredient);
-
-  // $(document).on("change", "select", doSelect);
-
-  // function doSelect() {
-  //   alert('clicked!');
-  //   $("select option:selected").hide;
-  //   // $(this).attr('selected');
-  //   // var IngredientId = $(this).attr('data-id');
-  // }
-
-  function selectID() {
-  $(document).on( "select" )
-  .change(function() {
-    $( "select option:selected" ).each(function() {
-      var selectedId = $(this).attr("data-id");
-      
-    });
-    
-  })
-  .trigger( "change" ); 
-}
-
   var i = 1;
   function addRecipeIngredient () {
     i++;
-  //   $("#RecipeIngredientGroup").append( `
-  //   <div class="form-row">
-  //   <div class="form-group col-md-3 ingredientCol" >
-  //   <label for="RecipeIngredient-${i}">Ingredient ${i}</label>
-  //   <input type="text" class="form-control" id="RecipeIngredient-${i}" placeholder="">
-  //   </div>
-
-  //   <div class="form-group col-md-3 AmountForSmallCol">
-  //   <label for="AmountForSmall-${i}">Amount for Small </label>
-  //   <input type="text" class="form-control" id="AmountForSmall-${i}" placeholder="">
-  // </div>
-
-  // <div class="form-group col-md-3 AmountForMediumCol">
-  //   <label for="AmountForMedium-${i}">Amount for Medium </label>
-  //   <input type="text" class="form-control" id="AmountForMedium-${i}" placeholder="">
-  // </div>
-
-  // <div class="form-group col-md-3 AmountForLargeCol">
-  //   <label for="AmountForLarge-${i}">Amount for Large </label>
-  //   <input type="text" class="form-control" id="AmountForLarge-${i}" placeholder="">
-  // </div>
-  // </div>` );
   $( "#RecipeIngredientGroup .form-row:first-child" ).clone().appendTo( "#RecipeIngredientGroup" );
 
   }
@@ -65,21 +21,14 @@ function insertRecipe(event) {
       event.preventDefault();
       RecipeIngredientsInputs = [];
       $('#RecipeIngredientGroup .form-row').each(function() {
-
-        selectID(execute);
-
-        
-        function execute() {
         var inputs = {
           // IngredientId:$(this).find('.ingredientCol select option:selected').attr('data-id'),
-          IngredientId: selectedId,
+          IngredientId: $(this).find('.ingredientCol select option:selected').attr('data-id'),
           AmountForSmall: $(this).find('.AmountForSmallCol input').val().trim(),
           AmountForMedium: $(this).find('.AmountForMediumCol input').val().trim(),
           AmountForLarge: $(this).find('.AmountForLargeCol input').val().trim()
         }
         RecipeIngredientsInputs.push(inputs);
-      }
-
       });
       var recipe = {
         RecipeName: $RecipeNameInput.val().trim(),
@@ -87,8 +36,6 @@ function insertRecipe(event) {
         RecipeType: $RecipeTypeInput.val().trim(),
         RecipeIngredients: RecipeIngredientsInputs
       };
-
-      console.log(recipe);
    
       // Send the POST request.
       $.ajax("/recipes/new", {
