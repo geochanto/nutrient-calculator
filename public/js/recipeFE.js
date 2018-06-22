a25bb7d1f6bd0ebb0573bec86ecd0f850dd23e96
George Chanturidze <georgechanturidze@Georges-MacBook-Pro.local>
2018-06-21 20:14:50 -0700

    adding changes

--- public/js/recipeFE.js
+++ public/js/recipeFE.js
@@ -9,32 +9,54 @@ $(document).ready(function () {
 
   $(document).on("click", "#addRecipeIngredient", addRecipeIngredient);
 
-  var i = 1;
+  // $(document).on("change", "select", doSelect);
 
+  // function doSelect() {
+  //   alert('clicked!');
+  //   $("select option:selected").hide;
+  //   // $(this).attr('selected');
+  //   // var IngredientId = $(this).attr('data-id');
+  // }
+
+  function selectID() {
+  $(document).on( "select" )
+  .change(function() {
+    $( "select option:selected" ).each(function() {
+      var selectedId = $(this).attr("data-id");
+      
+    });
+    
+  })
+  .trigger( "change" ); 
+}
+
+  var i = 1;
   function addRecipeIngredient () {
     i++;
-    $("#RecipeIngredientGroup").append( `
-    <div class="form-row">
-    <div class="form-group col-md-3 ingredientCol" >
-    <label for="RecipeIngredient-${i}">Ingredient ${i}</label>
-    <input type="text" class="form-control" id="RecipeIngredient-${i}" placeholder="">
-    </div>
+  //   $("#RecipeIngredientGroup").append( `
+  //   <div class="form-row">
+  //   <div class="form-group col-md-3 ingredientCol" >
+  //   <label for="RecipeIngredient-${i}">Ingredient ${i}</label>
+  //   <input type="text" class="form-control" id="RecipeIngredient-${i}" placeholder="">
+  //   </div>
 
-    <div class="form-group col-md-3 AmountForSmallCol">
-    <label for="AmountForSmall-${i}">Amount for Small </label>
-    <input type="text" class="form-control" id="AmountForSmall-${i}" placeholder="">
-  </div>
+  //   <div class="form-group col-md-3 AmountForSmallCol">
+  //   <label for="AmountForSmall-${i}">Amount for Small </label>
+  //   <input type="text" class="form-control" id="AmountForSmall-${i}" placeholder="">
+  // </div>
 
-  <div class="form-group col-md-3 AmountForMediumCol">
-    <label for="AmountForMedium-${i}">Amount for Medium </label>
-    <input type="text" class="form-control" id="AmountForMedium-${i}" placeholder="">
-  </div>
+  // <div class="form-group col-md-3 AmountForMediumCol">
+  //   <label for="AmountForMedium-${i}">Amount for Medium </label>
+  //   <input type="text" class="form-control" id="AmountForMedium-${i}" placeholder="">
+  // </div>
 
-  <div class="form-group col-md-3 AmountForLargeCol">
-    <label for="AmountForLarge-${i}">Amount for Large </label>
-    <input type="text" class="form-control" id="AmountForLarge-${i}" placeholder="">
-  </div>
-  </div>` );
+  // <div class="form-group col-md-3 AmountForLargeCol">
+  //   <label for="AmountForLarge-${i}">Amount for Large </label>
+  //   <input type="text" class="form-control" id="AmountForLarge-${i}" placeholder="">
+  // </div>
+  // </div>` );
+  $( "#RecipeIngredientGroup .form-row:first-child" ).clone().appendTo( "#RecipeIngredientGroup" );
+
   }
   
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
@@ -44,23 +66,20 @@ function insertRecipe(event) {
       RecipeIngredientsInputs = [];
       $('#RecipeIngredientGroup .form-row').each(function() {
 
-        $(document).on("click", "option", doSelect);
+        selectID(execute);
 
-        function doSelect() {
-          $(this).attr('selected');
-          // var IngredientId = $(this).attr('data-id');
-        }
-
-
+        
+        function execute() {
         var inputs = {
           // IngredientId:$(this).find('.ingredientCol select option:selected').attr('data-id'),
-          IngredientId:$(this).find('.ingredientCol select option:selected').attr('data-id'),
+          IngredientId: selectedId,
           AmountForSmall: $(this).find('.AmountForSmallCol input').val().trim(),
           AmountForMedium: $(this).find('.AmountForMediumCol input').val().trim(),
           AmountForLarge: $(this).find('.AmountForLargeCol input').val().trim()
         }
         RecipeIngredientsInputs.push(inputs);
-        console.log('inputs: ' + inputs.IngredientId);
+      }
+
       });
       var recipe = {
         RecipeName: $RecipeNameInput.val().trim(),
